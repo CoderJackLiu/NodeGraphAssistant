d9e936da9969aded379c465b0fbc559fdbae43d6
JackMaDrinkCoffee <596770421@qq.com>
2019-12-09 10:06:41 +0800

    +init!

---
+++ Source/NodeGraphProject/NodeGraphProjectGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "NodeGraphProjectGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class NODEGRAPHPROJECT_API ANodeGraphProjectGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
